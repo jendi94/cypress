bb7b00e4401a0941a775be0384a8abf0f52253da
jendi94 <jedrek_antkiewicz@yahoo.pl>
2022-03-10 16:32:21 +0100

    Create qm-test.spec.js

---
+++ qm-test.spec.js
@@ -0,0 +1,21 @@
+// test-qm.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+require('@cypress/snapshot').register()
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
+
+describe('QM Kontakt Test', () => {
+  it('Tests the kontakt page', () => {
+    cy.visit('www.qualityminds.de/')
+    cy.contains('Kontakt').click().snapshot()
+    cy.contains('hello@qualityminds.de').should('exist')
+
+  })
+})
